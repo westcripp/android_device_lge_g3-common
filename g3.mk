771d151934e2f2740432da3369e8f82839386fba
westcripp <korukaltan@gmail.com>
2015-02-14 00:28:00 +0200

    change dpi to 534
    Change-Id: I32c24b7ccfdf02eb39c53f75a5ca66af36dc766e

--- g3.mk
+++ g3.mk
@@ -54,8 +54,8 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
 
 # Screen density
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi 532dpi xxxhdpi
-PRODUCT_AAPT_PREF_CONFIG := 532dpi
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi 534dpi xxxhdpi
+PRODUCT_AAPT_PREF_CONFIG := 534dpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
